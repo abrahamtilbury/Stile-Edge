9c464c47240d77cfa6c93390cea9578ecbc9186f
abrahamtilbury <abrahamtilbury@gmail.com>
2026-08-10 20:52:00 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+const menuButton = document.querySelector('.menu-button');
+const menu = document.querySelector('.mobile-menu');
+
+menuButton.addEventListener('click', () => {
+    menu.classList.toggle('open');
+});
