1c96c3066e78294524cd6a50c0058c523af3c5bb
abrahamtilbury <abrahamtilbury@gmail.com>
2026-08-11 18:37:49 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,63 @@ const menu = document.querySelector('.mobile-menu');
 menuButton.addEventListener('click', () => {
     menu.classList.toggle('open');
 });
+
+const heroImage = document.querySelector('.hero-image');
+const previousButton = document.querySelector('.hero-arrow-left');
+const nextButton = document.querySelector('.hero-arrow-right');
+const heroDots = document.querySelectorAll('.hero-dots button');
+const heroImages = [
+    'images/hero-1.jpg',
+    'images/hero-2.jpg',
+    'images/hero-3.jpg'
+];
+
+let currentSlide = 0;
+
+
+function showSlide(index) {
+    currentSlide = index;
+    heroImage.src = heroImages[currentSlide];
+    heroDots.forEach((dot, index) => {
+        dot.classList.toggle(
+            'active',
+            index === currentSlide
+        );
+    });
+}
+
+
+function nextSlide() {
+    currentSlide++;
+    if (currentSlide >= heroImages.length) {
+        currentSlide = 0;
+    }
+
+    showSlide(currentSlide);
+}
+
+
+function previousSlide() {
+
+    currentSlide--;
+
+    if (currentSlide < 0) {
+        currentSlide = heroImages.length - 1;
+    }
+
+    showSlide(currentSlide);
+}
+
+
+nextButton.addEventListener('click', nextSlide);
+
+previousButton.addEventListener('click', previousSlide);
+
+
+heroDots.forEach((dot, index) => {
+
+    dot.addEventListener('click', () => {
+        showSlide(index);
+    });
+
+});
