d902be7db36cfd37f4529827042531a415d86d7a
abrahamtilbury <abrahamtilbury@gmail.com>
2026-08-17 13:11:49 +0800

    Add click event to toggle project card flip

--- script.js
+++ script.js
@@ -96,5 +96,11 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
+    document.querySelectorAll('.project-card').forEach(card => {
+  card.addEventListener('click', () => {
+    card.classList.toggle('is-flipped');
+  });
+});
+
     startAutoplay();
 })();
